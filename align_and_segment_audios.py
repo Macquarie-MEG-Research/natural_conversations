8de27d5d81cfc79a29b030c80198e54c213d778b
Yifan Gu <yigu8115@gmail.com>
2023-12-12 16:50:45 +1100

    Add plot_all_data_together

--- align_and_segment_audios.py
+++ align_and_segment_audios.py
@@ -17,6 +17,11 @@ import speech_recognition
 import glob
 import pandas as pd
 import copy
+import whisper
+# from openai import OpenAI
+import meegkit
+from PIL import Image
+from matplotlib import cm
 
 # min_speech_segment_gap: second. Any two segments separated less than this should be grouped into a single segment
 # min_speech_segment_length: second. Any utterance should be longer than this
@@ -30,6 +35,9 @@ vad_pipeline = Pipeline.from_pretrained(
     use_auth_token="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")
 
 print(vad_pipeline)
+
+whisper_model = whisper.load_model("small.en")
+print(whisper_model)
 
 
 def _get_subject_dirs(subject_MEG, local_dir = "./my_folder/"):
@@ -61,26 +69,34 @@ def get_data_for_subject(subject_MEG, local_dir = "./my_folder/"):
   for task in tasks:
     task_data_files, subject_dirs = get_data_for_subject_task(subject_MEG, task, local_dir)
     subject_data_file_list.append(task_data_files)
-  
+
   return subject_data_file_list, subject_dirs
 
 def get_data_for_subject_task(subject_MEG, task, local_dir = "./my_folder/"):
   # subject_MEG: e.g., "G02"
 
   subject_dirs = _get_subject_dirs(subject_MEG, local_dir)
-  print(subject_dirs)
+  # print(subject_dirs)
 
   fname_raw = glob.glob(subject_dirs["meg_dir"] + "*" + task + ".con")[0]
-  # fname_elp = glob.glob(meg_dir + "*.elp")
-  # fname_hsp = glob.glob(meg_dir + "*.hsp")
-  # fname_mrk = glob.glob(meg_dir + "*.mrk")
+  fname_elp = glob.glob(subject_dirs["meg_dir"] + "*.elp")[0]
+  fname_hsp = glob.glob(subject_dirs["meg_dir"] + "*.hsp")[0]
+  fname_mrk = glob.glob(subject_dirs["meg_dir"] + "*_final.mrk")[0]
+  fname_eeg = glob.glob(subject_dirs["eeg_dir"] + "*" + task + ".eeg")[0]
+  fname_vhdr = glob.glob(subject_dirs["eeg_dir"] + "*" + task + ".vhdr")[0]
+
 
   # .WAV audio data
   interviewer_audio = glob.glob(subject_dirs["audio_dir"] + "console_mic*" + task + ".wav")[0]
   participant_audio = glob.glob(subject_dirs["audio_dir"] + "subject_mic*" + task + ".wav")[0]
 
-  return {"meg_file":fname_raw, 
-                  "interviewer_audio_file": interviewer_audio, 
+  return {"meg_file":fname_raw,
+          "meg_file_elp":fname_elp,
+          "meg_file_hsp":fname_hsp,
+          "meg_file_mrk":fname_mrk,
+          "eeg_file":fname_eeg,
+          "eeg_file_vhdr":fname_vhdr,
+                  "interviewer_audio_file": interviewer_audio,
                   "participant_audio_file": participant_audio}, subject_dirs
 
 
@@ -126,6 +142,42 @@ def _extract_meg_audio(meg_file, output_dir):
 
   print("Done", flush=True)
   return output_file_interviewer, output_file_participant
+
+
+def load_meg_data(subject_task_data):
+
+  # Raw extraction ch misc 23-29 = triggers
+  # ch misc 007 = audio
+  raw = mne.io.read_raw_kit(
+      subject_task_data["meg_file"],
+      mrk=subject_task_data["meg_file_mrk"],
+      elp=subject_task_data["meg_file_elp"],
+      hsp=subject_task_data["meg_file_hsp"],
+      stim=[*[166], *range(176, 190)],
+      slope="+",
+      stim_code="channel",
+      stimthresh=2,  # 2 for adult (1 for child??)
+      preload=True,
+      allow_unknown_format=False,
+      # verbose=True,
+  )
+
+  return raw
+
+
+def denoise_meg_data(raw):
+  # Apply TSPCA for noise reduction
+  noisy_data = raw.get_data(picks="meg").transpose()
+  noisy_ref = raw.get_data(picks=[160,161,162]).transpose()
+  data_after_tspca, idx = meegkit.tspca.tsr(noisy_data, noisy_ref)[0:2]
+  raw._data[0:160] = data_after_tspca.transpose()
+
+  return raw
+
+def filter_meg_data(raw, l_freq=1, h_freq=40):
+  # raw = my_preprocessing.reject_artefact(raw, 1, 40, False, '')
+  raw.filter(l_freq=l_freq, h_freq=h_freq)
+  return raw
 
 
 def plot_audio_alignment(audio_file_ref, audio_file_delayed, offset_t, peak_corr, plot_sec=60):
@@ -232,7 +284,7 @@ def postprocess_segments(segments_raw, min_speech_segment_gap=MIN_SPEECH_SEGMENT
 
   segments = copy.deepcopy(segments_raw)
 
-  
+
   # merge segments separated by too small a gap
   i_merge_with_previous = []
   for i, seg in enumerate(segments):
@@ -285,9 +337,21 @@ test_postprocess_segments()
 
 def _get_segments_file_raw(audio_file):
    return audio_file.replace('.wav', '_segments_raw.csv').replace('audios', 'audios_metadata')
-   
+
 def _get_segments_file(audio_file):
    return audio_file.replace('.wav', '_segments.csv').replace('audios', 'audios_metadata')
+
+def _get_transcript_file(audio_file):
+   return audio_file.replace('.wav', '_segments_with_transcript.csv').replace('audios', 'audios_metadata')
+
+def _save_transcript_file(segments_with_transcript, output_file):
+  with open(output_file, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows([["index", "start", "end", "text"]])
+    for ind, seg in enumerate(segments_with_transcript):
+      writer.writerows([[ind, seg[0], seg[1], seg[2]]])
+  return output_file
+
 
 def postprocess_and_save_segment(segments_raw, audio_file):
   segments_processed = postprocess_segments(segments_raw)
@@ -349,27 +413,14 @@ def export_audio_segments(segments, audio_file):
 
 
 def _transcribe_speech(segments, audio_file):
-  recognizer = speech_recognition.Recognizer()
-  speech_texts = []
-  for seg_index, seg in enumerate(segments):
+  segments_with_transcripts = copy.deepcopy(segments)
+  for seg_index, seg in enumerate(segments_with_transcripts):
     seg_audio_file = audio_file.replace(".wav", f"_seg_{seg_index:0>4}.wav")
-    with speech_recognition.AudioFile(seg_audio_file) as source:
-      try:
-        audio_seg = recognizer.record(source)
-        t_current = seg[1]
-        # speech_text = recognizer.recognize_google(audio_seg)
-        speech_text = recognizer.recognize_whisper_api(audio_seg)
-        # recognize_sphinx
-        # recognize_wit
-        print(f"[Segment #{seg_index:0>4}] between {seg[0]:.3f} and {seg[1]:.3f}:")
-        print(speech_text)
-        speech_texts.append(speech_text)
-      except:
-        # print(' ')
-        speech_texts.append('')
-        continue
-    return speech_texts
-
+    transcript = whisper_model.transcribe(seg_audio_file)
+    print(f"[Segment #{seg_index:0>4}] between {seg[0]:.3f} and {seg[1]:.3f}:")
+    print(transcript["text"])
+    segments_with_transcripts[seg_index].append(transcript["text"])
+  return segments_with_transcripts
 
 
 def audio_processing_pipeline(data_files, processing_dir, plot=False, force_align=False):
@@ -400,20 +451,37 @@ def audio_processing_pipeline(data_files, processing_dir, plot=False, force_alig
 
     if os.path.isfile(_get_segments_file(audio_file)):
       print("skipping speech segmentation (results already exist) for: %s" % audio_file, flush=True)
+      segments_file = _get_segments_file(audio_file)
+      segments = read_segments(segments_file)
     else:
       segments = _segement_speech(left_channel_audio_file, plot=plot)
       postprocess_and_save_segment(segments, audio_file)
 
-    # _export_audio_segments(segments, left_channel_audio_file)
+    if os.path.isfile(_get_transcript_file(audio_file)):
+      print("skipping speech-to-text (results already exist) for: %s" % audio_file, flush=True)
+    else:
+      export_audio_segments(segments, left_channel_audio_file)
+      segments_with_transcripts = _transcribe_speech(segments, left_channel_audio_file)
+      transcript_file = _get_transcript_file(audio_file)
+      _save_transcript_file(segments_with_transcripts, transcript_file)
 
-    # speech_texts = _transcribe_speech(segments, left_channel_audio_file)
+def plot_meg_layout(data):
+  layout = mne.channels.find_layout(data.info)
+  layout.plot()
+  # print(layout.pos.shape)
+
+  fig, axs = plt.subplots(1, 1, sharex='all') #, figsize=(12, 10))
+  x = layout.pos[0:160,0]
+  y = layout.pos[0:160,1]
+
+  axs.plot(x, y, "o--")
 
 
 def get_all_subjects():
   # returns ['G01", 'G02", ...]
   all_subjects = []
   return [f"G{i:0>2}" for i in np.arange(1,33)]
-    
+
 
 def re_process_segments(subject, local_dir = "./my_folder/"):
   # Run this if min_speech_segment_gap or min_speech_segment_length is changed
@@ -430,7 +498,6 @@ def re_process_segments(subject, local_dir = "./my_folder/"):
 
 
 def load_processed_audio_data(subject, task, local_dir = "./my_folder/", inspect_alignment=False, inspect_segmentation=False):
-  # example: load_processed_audio_data("G02", "B1", inspect_alignment=True, inspect_segmentation=True)
   task_data_files, subject_dir = get_data_for_subject_task(subject, task, local_dir)
 
   processing_dir=subject_dir["audio_dir_processing"]
@@ -446,7 +513,7 @@ def load_processed_audio_data(subject, task, local_dir = "./my_folder/", inspect
     output["interviewer_audio_file_meg"] = interviewer_audio_file_meg
     output["participant_audio_file_meg"] = participant_audio_file_meg
 
-  for prefix, audio_file, meg_audio_file in [("interviewer_", interviewer_audio_file_raw, interviewer_audio_file_meg),("participant_", participant_audio_file_raw, participant_audio_file_meg)]:
+  for prefix, audio_file in [("interviewer_", interviewer_audio_file_raw),("participant_", participant_audio_file_raw)]:
     left_channel_audio_file = _keep_left_channel_only(audio_file, processing_dir)
     offset_file = _get_offset_file(audio_file)
     offset_t, peak_corr = read_offset(offset_file)
@@ -456,7 +523,7 @@ def load_processed_audio_data(subject, task, local_dir = "./my_folder/", inspect
     segments_file = _get_segments_file(audio_file)
     segments = read_segments(segments_file)
     output[prefix+"segments"] = segments
-  
+
   if inspect_alignment:
     for prefix in ["interviewer_", "participant_"]:
       plot_audio_alignment(output[prefix + "audio_file_meg"], output[prefix+"audio_file"], output[prefix+"offset_t"], output[prefix+"peak_corr"], plot_sec=60)
@@ -466,4 +533,185 @@ def load_processed_audio_data(subject, task, local_dir = "./my_folder/", inspect
       export_audio_segments(output[prefix+"segments"], output[prefix+"audio_file"])
 
   return output
-   
+
+
+
+def find_meg_trigger(meg_data, task):
+  task_to_trigger_channel_mapping = {
+      'B1': 181,
+      'B2': 182,
+      'B3': 183,
+      'B4': 184,
+      'B5': 185,
+  }
+  trigger_ch = task_to_trigger_channel_mapping[task]
+  trigger_data = meg_data._data[trigger_ch,:]
+  std = np.std(trigger_data)
+  mean = np.mean(trigger_data)
+  trigger_onset = np.nonzero(trigger_data > mean+3*std)[0][0]
+  return trigger_onset
+
+def find_eeg_trigger(eeg_data):
+  eeg_events, _ = mne.events_from_annotations(eeg_data)
+  print("EEG events for trigger:")
+  print(eeg_events)
+  trigger_onset = 0
+  for event in eeg_events:
+    if event[0] > 0:
+      trigger_onset = event[0]
+
+  return trigger_onset
+
+
+def load_eeg_data(data_file):
+  # Read raw EEG data
+  raw_eeg = mne.io.read_raw_brainvision(data_file["eeg_file_vhdr"], preload=True)
+
+  # set channel types explicitly as these are not read in automatically
+  raw_eeg.set_channel_types({'32': 'ecg', '63': 'eog'})
+
+  # Filtering & ICA
+  # raw_eeg = my_preprocessing.reject_artefact(raw_eeg, 1, 40, False, '')
+
+  return raw_eeg
+
+def normalize_data_for_plot(data):
+  # zero mean
+  color_data = data - data.mean()
+
+  # normalize range to be within [0, 1]
+  # (1) keep the mean at 0.5 (so that mean is plot as white)
+  # (2) make sure at least one of min/max is at either 0 or 1 to fully utilize the colormap (max contrast)
+  min = color_data.min()
+  max = color_data.max()
+  if abs(min) >= max:
+    color_data = ((color_data / abs(min) ) + 1)/2
+    assert color_data.min() == 0
+  else:
+    color_data = ((color_data / max ) - 1)/2 + 1
+    assert color_data.max() == 1
+  return color_data
+
+def plot_meg_data_as_image(data, samplerate=10):
+  plot_every_ms = int(1000/samplerate)
+  color_data = data._data[0:160, 0::plot_every_ms]
+
+  color_data = normalize_data_for_plot(color_data)
+  # Creates PIL image
+  img = Image.fromarray(np.uint8(cm.seismic(color_data)*255))
+  return img, color_data
+
+def pad_color_data(meg_color_data, eeg_color_data):  
+  meg_length = len(meg_color_data[0,:])
+  eeg_length = len(eeg_color_data[0,:])
+  merged_length = max(meg_length, eeg_length)
+  print("meg_length %s  eeg_length %s" % (meg_length, eeg_length))
+  if merged_length - meg_length > 0:
+    meg_color_data = np.append(
+        meg_color_data, 
+        np.ones((len(meg_color_data[:,0]), merged_length - meg_length))*0.5,
+        axis=1)
+  elif merged_length - eeg_length > 0:
+    eeg_color_data = np.append(
+        eeg_color_data, 
+        np.ones((len(eeg_color_data[:,0]), merged_length - eeg_length))*0.5,
+        axis=1)
+  print("meg_color_data shape: " )
+  print(meg_color_data.shape)
+  print("eeg_color_data shape: ")
+  print(eeg_color_data.shape)
+
+  return meg_color_data, eeg_color_data, merged_length
+
+def generate_segment_color_data(audio_data, samplerate, length):
+  plot_every_ms = int(1000/samplerate)
+  time = np.arange(length) * plot_every_ms
+  segment_line_with = 5
+  segment_color_data_list = []
+  for segments, offset_t, color in [(audio_data["participant_segments"], audio_data["participant_offset_t"], 0.7),
+   (audio_data["interviewer_segments"], audio_data["interviewer_offset_t"], 0.3)]:
+    segment_data = np.ones(len(time)) * 0.5
+    for seg in segments:
+      t_start = seg[0] - offset_t
+      t_end = seg[1] - offset_t
+      segment_data[int(t_start*samplerate):int(t_end*samplerate)] = color
+    segment_color_data_list.append(np.tile(segment_data, (segment_line_with, 1)))
+
+  segment_color_data = np.append(segment_color_data_list[0], segment_color_data_list[1], axis=0)
+
+  return segment_color_data
+
+def extract_meg_eeg_colordata(task,meg_data,eeg_data,samplerate):
+  plot_every_ms = int(1000/samplerate)
+
+  meg_trigger_onset = find_meg_trigger(meg_data, task)
+  eeg_trigger_onset = find_eeg_trigger(eeg_data)
+  assert eeg_trigger_onset > 0, "eeg_trigger_onset %s should not be zero"
+
+  print("meg_trigger_onset: %s, eeg_trigger_onset: %s" % (meg_trigger_onset, eeg_trigger_onset))
+
+  meg_color_data = meg_data._data[0:160, 0::plot_every_ms]
+  meg_color_data = normalize_data_for_plot(meg_color_data)
+  if  eeg_trigger_onset > meg_trigger_onset:
+    # trim eeg data (we don't pad meg data because audio segments are aligned with meg timeline)
+    eeg_color_data = eeg_data._data[0:63, int(eeg_trigger_onset-meg_trigger_onset)::plot_every_ms]
+    eeg_color_data = normalize_data_for_plot(eeg_color_data)
+  else:
+    # pad eeg data
+    eeg_color_data = eeg_data._data[0:63, 0::plot_every_ms]
+    eeg_color_data = normalize_data_for_plot(eeg_color_data)
+    eeg_pad_length = int(np.round((meg_trigger_onset - eeg_trigger_onset)/plot_every_ms))
+    print("eeg_pad_length: %s" % eeg_pad_length)
+    eeg_color_data = np.append(
+        np.ones((63, eeg_pad_length))*0.5,
+        eeg_color_data, 
+        axis=1)
+
+  print("meg_color_data.shape:")
+  print(meg_color_data.shape)
+
+  print("eeg_color_data.shape:")
+  print(eeg_color_data.shape)
+
+  meg_color_data, eeg_color_data, length = pad_color_data(
+      meg_color_data, eeg_color_data)
+
+  return meg_color_data, eeg_color_data, length
+
+
+
+def plot_data_with_speech_segmentation_as_image(task,meg_data,eeg_data,audio_data,
+                                                samplerate=100):
+  meg_color_data, eeg_color_data, length = extract_meg_eeg_colordata(task,meg_data,eeg_data,
+                                                samplerate)
+
+  # plot meg data + segments first
+  segment_color_data = generate_segment_color_data(audio_data, samplerate, length)
+  
+  color_data = np.append(meg_color_data, segment_color_data, axis=0)
+
+  # plot time ticks
+  time_second_tick = np.ones(length) * 0.5
+  time_second_tick[::samplerate] = 0.7
+  time_tick_color_data = np.tile(time_second_tick, (4, 1))
+  color_data = np.append(color_data, time_tick_color_data, axis=0)
+
+  # plot eeg 
+  color_data = np.append(color_data, eeg_color_data, axis=0)
+
+  # Creates PIL image
+  img = Image.fromarray(np.uint8(cm.seismic(color_data)*255))
+  return img
+
+
+def plot_all_data_together(subject, task, local_dir="./my_folder/"):
+  data_file, subject_dirs = get_data_for_subject_task(subject, task, local_dir)
+  meg_data = load_meg_data(data_file)
+  meg_data = denoise_meg_data(meg_data)
+  meg_data = filter_meg_data(meg_data)
+  audio_data = load_processed_audio_data(subject, task)
+  eeg_data = load_eeg_data(data_file)
+
+  img = plot_data_with_speech_segmentation_as_image(task, meg_data, eeg_data, audio_data)
+  return img
+
