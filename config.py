ff67b116f3cb95e0952494c4286086a918351c49
Eric Larson <larson.eric.d@gmail.com>
2024-01-29 15:31:21 -0500

    WIP: More progress

--- config.py
+++ config.py
@@ -20,30 +20,32 @@ task = "conversation"
 subjects = ["01"]  # "all" TODO: Process all
 runs = ["01", "02", "03", "04", "05", "06"]
 
-use_maxwell_filter = False
-# find_flat_channels_meg = True  # TODO: Enable for files w/o cal + cross-talk
-# find_noisy_channels_meg = True
-# mf_head_origin = (0., 0., 0.04)
 ch_types = ["meg", "eeg"]
 data_type = "meg"
+eeg_reference = "average"
 
 l_freq = None
 h_freq = 40.0
 epochs_decim = 5
 process_rest = True
 
-reject = "autoreject_global"
-conditions = ["ba", "da", "dummy"]  # dummy just to make event processing happy
-epochs_tmin = -0.2
-epochs_tmax = 0.5
-baseline = (None, 0)
+regress_artifact = dict(picks="meg", picks_artifact=["MISC 001", "MISC 002", "MISC 003"])
+
 spatial_filter = 'ssp'
 n_proj_ecg = dict(n_mag=1, n_eeg=0)
 n_proj_eog = dict(n_mag=1, n_eeg=1)
 
-run_source_estimation = False
+reject = {'eeg': 200e-6, 'mag': 2000e-15}
+conditions = ["ba", "da", "dummy"]  # dummy just to make event processing happy
+epochs_tmin = -0.2
+epochs_tmax = 0.5
+baseline = (None, 0)
+
+run_source_estimation = True
+subjects_dir = bids_root / "derivatives" / "freesurfer" / "subjects"
+use_template_mri = "fsaverage"
+adjust_coreg = True  # TODO: Add option to use manual -trans.fif files
 spacing = "oct6"
 mindist = 1
-source_info_path_update = {"processing": "clean", "suffix": "epo"}
 inverse_method = "dSPM"
-noise_cov = "rest"
+noise_cov = (None, 0.0)
